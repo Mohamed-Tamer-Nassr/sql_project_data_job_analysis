833b54c2a9253f58a51b70558f795e49ae6650c8
Mohamed Tamer <mohamed.tamer.mtn@gmail.com>
2025-10-29 11:12:49 +0300

    second query added --version 0.2

--- project_sql/2_top_paying_jobs_skills.sql
+++ project_sql/2_top_paying_jobs_skills.sql
@@ -1,10 +1,5 @@
 /*
-What are the top-paying jobs for my role?
 What are the skills required for these top-paying roles?
-What are the most in-demand skills for my role?
-What are the top skills based on salary for my role?
-What are the most optimal skills to learn?
- Optimal: Defined as skills that are both High Demand AND High Paying.
 */
 
 WITH top_paying_jobs AS (
